f89b39381260b1ff0409ffddfab95d85c877fc3b
NIkolayrr <nikolay.r.rangelov@gmail.com>
2021-05-31 22:56:39 +0300

    cleanup

--- app/screens/welcome/welcome-screen.tsx
+++ app/screens/welcome/welcome-screen.tsx
@@ -20,65 +20,7 @@ const TEXT: TextStyle = {
   fontFamily: typography.primary,
   fontSize: 20,
 }
-const BOLD: TextStyle = { fontWeight: "bold" }
-const HEADER: TextStyle = {
-  paddingTop: spacing[3],
-  paddingBottom: spacing[4] + spacing[1],
-  paddingHorizontal: 0,
-}
-const HEADER_TITLE: TextStyle = {
-  ...TEXT,
-  ...BOLD,
-  fontSize: 12,
-  lineHeight: 15,
-  textAlign: "center",
-  letterSpacing: 1.5,
-}
-const TITLE_WRAPPER: TextStyle = {
-  ...TEXT,
-  textAlign: "center",
-}
-const TITLE: TextStyle = {
-  ...TEXT,
-  ...BOLD,
-  fontSize: 28,
-  lineHeight: 38,
-  textAlign: "center",
-}
-const ALMOST: TextStyle = {
-  ...TEXT,
-  ...BOLD,
-  fontSize: 26,
-  fontStyle: "italic",
-}
-const BOWSER: ImageStyle = {
-  alignSelf: "center",
-  marginVertical: spacing[5],
-  maxWidth: "100%",
-}
-const CONTENT: TextStyle = {
-  ...TEXT,
-  color: "#BAB6C8",
-  fontSize: 15,
-  lineHeight: 22,
-  marginBottom: spacing[5],
-}
-const CONTINUE: ViewStyle = {
-  paddingVertical: spacing[4],
-  paddingHorizontal: spacing[4],
-  backgroundColor: "#5D2555",
-}
-const CONTINUE_TEXT: TextStyle = {
-  ...TEXT,
-  ...BOLD,
-  fontSize: 13,
-  letterSpacing: 2,
-}
 const FOOTER: ViewStyle = { backgroundColor: "#20162D", marginBottom: 64 }
-const FOOTER_CONTENT: ViewStyle = {
-  paddingVertical: spacing[4],
-  paddingHorizontal: spacing[4],
-}
 
 export const WelcomeScreen = observer(function WelcomeScreen() {
   const navigation = useNavigation()
@@ -195,9 +137,7 @@ export const WelcomeScreen = observer(function WelcomeScreen() {
         <Text style={TEXT}>Гласувай електронно!</Text>
       </Screen>
       <View style={FOOTER}>
-        <View style={FOOTER_CONTENT}>
-          <Button text="Sign in with Google" onPress={() => googleSignIn()} />
-        </View>
+        <Button preset="primary" text="Sign in with Google" onPress={() => googleSignIn()} />
       </View>
     </View>
   )
